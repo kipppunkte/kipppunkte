37bd636d30793846f7ea1a44ec013597465b805e
Fabien Farella <f.farella@ews-consulting.at>
2023-08-27 18:50:11 +0200

    added requirements

--- noxfile.py
+++ noxfile.py
@@ -9,19 +9,20 @@ nox.options.sessions = ['serve']
 nox.options.error_on_external_run = True
 nox.options.error_on_missing_interpreters = True
 
-
-@nox.session(python=False, venv_backend='none', reuse_venv=True)
-def lint(session):
-    venvDir.joinpath("bin/python")
+def refresh_deps(session):
     pip = venvDir.joinpath("bin/pip")
     session.run(str(pip), "install", "-r", "requirements.txt")
 
+
+
 @nox.session(python=False, venv_backend='none', reuse_venv=True)
 def serve(session):
+    refresh_deps(session)
     mkdocs = venvDir.joinpath("bin/mkdocs")
     session.run(str(mkdocs), "serve")
 
 @nox.session(python=False, venv_backend='none', reuse_venv=True)
 def deploy(session):
+    refresh_deps(session)
     mkdocs = venvDir.joinpath("bin/mkdocs")
     session.run(str(mkdocs), "gh-deploy")
