fc9b12cbe12a750b6e997f13af0d4d871acd751f
Fabien Farella <f.farella@ews-consulting.at>
2023-08-27 23:47:52 +0200

    stations

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import logging
 
 import nox
 
@@ -21,6 +22,15 @@ def serve(session):
     refresh_deps(session)
     mkdocs = venvDir.joinpath("bin/mkdocs")
     session.run(str(mkdocs), "serve")
+
+
+@nox.session(python=False, venv_backend='none', reuse_venv=True)
+def build(session):
+    refresh_deps(session)
+    mkdocs = venvDir.joinpath("bin/mkdocs")
+    session.run(str(mkdocs), "build")
+
+
 
 @nox.session(python=False, venv_backend='none', reuse_venv=True)
 def deploy(session):
@@ -47,3 +57,67 @@ def urls(session):
     session.run("cat", "qr_codes.csv")
 
 
+
+DOCS_PATH = Path("./docs")
+ASSETS_PATH = DOCS_PATH / "assets"
+DATA_PATH = Path("GoogleDocs")
+PNG_FILES = sorted(DATA_PATH.glob("*.png"))
+
+
+TPL = r'''
+# Station #s_id#: #s_name#
+
+
+===+ "Auftrag"
+
+    ![Image title](assets/#image_url_a#){: style="max-height:60vh" }
+
+
+=== "Ergebniss"
+
+    ![Image title](assets/#image_url_b#){: style="max-height:60vh" }
+'''
+
+@nox.session(python=False, venv_backend='none', reuse_venv=True)
+def urls(session):
+    site = 'https://kipppunkte.github.io/kipppunkte'
+    stations = Path("stations.txt").read_text().split("\n")
+    stations = [_.split("\t", 1) for _ in stations if _.strip()]
+    res = []
+    toc = []
+    toc1 = []
+
+    for s_id, s_name in stations:
+        fname = DOCS_PATH / f"{s_id}.md"
+        png_fnames = sorted(
+            DATA_PATH.glob(f"{s_id}_*.png")
+        )
+        png0 = None
+        png1 = None
+        import shutil
+        text = TPL.replace("#s_id#", s_id).replace("#s_name#", s_name)
+        logging.info((s_id, s_name))
+        # logging.info(png_fnames)
+        if len(png_fnames) == 2:
+            png0 = ASSETS_PATH / png_fnames[0].name
+            shutil.copy2(png_fnames[0], png0)
+            png1 = ASSETS_PATH / png_fnames[1].name
+            shutil.copy2(png_fnames[1], png1)            
+            text = text.replace("#image_url_a#", png0.name).replace("#image_url_b#", png1.name)
+            # logging.info("Found 2 files.")
+        elif len(png_fnames) == 1:
+            png0 = ASSETS_PATH / png_fnames[0].name
+            shutil.copy2(png_fnames[0], png0)          
+            text = text.replace("#image_url_a#", png0.name).replace("#image_url_b#", png0.name)
+            logging.warn("Found just 1 file.")
+        else:
+            logging.error("Found no file.")
+        fname.write_text(text)
+        toc.append(
+            f'- "Station {s_id}: {s_name}": {fname.name}'
+        )
+        toc1.append(
+            f'- [Station {s_id}: {s_name}]({fname.name}) (n={len(png_fnames)})'
+        )
+    Path("staging/test.yaml").write_text('\n'.join(toc))
+    Path("staging/test1.md").write_text('\n'.join(toc1))
